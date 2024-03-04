7c090ec820e0b95675cd36f3dc95bc8c3fc44912
ooexiaoo <vpaherwar@gmail.com>
2024-03-04 23:06:46 +0530

    Chapter 14 - Route Groups

---
+++ hello-world/src/app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+export default function Register() {
+  return <div><h1>Register</h1>
+  <p>by adding paranthesis there is no need to add "auth" in the <strong>URL</strong></p>
+  </div>
+}
--- hello-world/src/app/not-found.tsx
+++ hello-world/src/app/not-found.tsx
@@ -3,6 +3,7 @@ export default function NotFound(){
     <div>
     <h2>Page not found</h2>
     <p>Could not find requested resource</p>
+    <img style={{width: 200}} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.tenor.com%2FWeLDYNcN4tsAAAAd%2Fwhat.gif&f=1&nofb=1&ipt=04591c3f83c347c432042d303bf6d99e0ddd50e6fdf7b6739793fa15a21ff46c&ipo=images" alt="What" />
     </div>
     );
 }
